100d3ded2d60a13fc6e45f4bc969062f531abbcb
Phu Hung Nguyen <1079742+PhuNH@users.noreply.github.com>
2019-05-23 22:44:54 +0200

    Try adding shp file in c++

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	g++ cpp-mapnik.cpp -I/usr/include/mapnik -std=c++11 -Wall -g -o cpp-mapnik -lmapnik -L/usr/lib64 -L/usr/lib -licuuc
+	g++ cpp-mapnik.cpp -I/usr/include/mapnik -I/usr/include/gdal -DBIGINT -std=c++11 -Wall -O2 -g -o cpp-mapnik -lmapnik -lgdal -L/usr/lib64 -L/usr/lib -licuuc
 clean:
 	rm cpp-mapnik
--- cpp-mapnik.cpp
+++ cpp-mapnik.cpp
@@ -3,6 +3,12 @@
 #include <mapnik/agg_renderer.hpp>
 #include <mapnik/load_map.hpp>
 #include <mapnik/image_util.hpp>
+#include <mapnik/feature_type_style.hpp>
+#include <mapnik/datasource.hpp>
+#include <mapnik/layer.hpp>
+
+#include <gdal.h>
+#include <ogrsf_frmts.h>
 
 using namespace mapnik;
 
@@ -24,9 +30,82 @@ void xml_filter() {
 
 int main() {
     std::string plugins_dir = "/usr/lib64/mapnik/input";
-    std::string srs = "+init=epsg:3857";
-    
     datasource_cache::instance().register_datasources(plugins_dir);
     
-    xml_filter();
+    //xml_filter();
+    
+    std::string xml_path = "cpp-shape-layers.xml",
+                base_data_path = "../data/world_merc.shp",
+                the_data_path = "../data/seis_cells.shp",
+                output_path = "../output-mapnik/cpp-shape-layers.png";
+    std::string srs_merc = "+init=epsg:3857";
+    
+    GDALAllRegister();
+    
+    GDALDataset * poDS = (GDALDataset *) GDALOpenEx(the_data_path.c_str(), GDAL_OF_VECTOR, NULL, NULL, NULL);
+    OGRLayer * poLayer = poDS->GetLayer(0);
+    std::vector<double> myData = std::vector<double>();
+    for (auto& poFeature: poLayer)
+        myData.push_back(poFeature->GetFieldAsDouble(0));
+    
+    int colorCount = 256;
+    double  maxData = *std::max_element(std::begin(myData), std::end(myData)),
+            minData = *std::min_element(std::begin(myData), std::end(myData));
+    double rangeOneColor = (maxData - minData) / colorCount;
+    std::cout << "max min " << maxData << " " << minData << " " << rangeOneColor << " data vector len " << myData.size() << std::endl;
+    std::vector<double> stops = std::vector<double>(colorCount+1);
+    for (int iStop = 0; iStop < colorCount+1; iStop++)
+        stops[iStop] = minData + iStop * rangeOneColor;
+    stops[colorCount] += 1;
+    
+    Map m(1300, 900);
+    //m.set_background(color("white"));
+    //load_map(m, xml_path);
+    
+    feature_type_style world_style;
+    rule world_rule;
+    polygon_symbolizer world_psym;
+    world_psym.properties[keys::fill] = color("#c7e9b4");
+    world_rule.append((std::move(world_psym)));
+    world_style.add_rule(std::move(world_rule));
+    m.insert_style("World Style", std::move(world_style));
+    
+    parameters wp;
+    wp["type"] = "shape";
+    wp["file"] = base_data_path;
+    std::shared_ptr<datasource> world_ds = datasource_cache::instance().create(wp);
+    layer wl("world_layer");
+    wl.set_srs(srs_merc);
+    wl.set_datasource(world_ds);
+    wl.add_style("World Style");
+    m.add_layer(wl);
+    
+    std::vector<double> stop = {0, 1, 2, 3, 4, 4.6};
+    feature_type_style s;
+    for (int iColor = 0; iColor < 5; iColor++) {
+        rule r;
+        std::ostringstream stringStream;
+        stringStream << "[Data] >= " << stop[iColor] << " and [Data] < " << stop[iColor+1];
+        expression_ptr f = parse_expression(stringStream.str());
+        r.set_filter(f);
+        polygon_symbolizer psym;
+        psym.properties[keys::fill] = color("red");
+        psym.properties[keys::fill_opacity] = static_cast<double>(iColor+1) / 5;
+        r.append(std::move(psym));
+        s.add_rule(std::move(r));
+    }
+    m.insert_style("data_style", std::move(s));
+    
+    parameters p;
+    p["type"] = "shape";
+    p["file"] = the_data_path;
+    std::shared_ptr<datasource> ds = datasource_cache::instance().create(p);
+    layer l("data_layer");
+    l.set_srs(srs_merc);
+    l.set_datasource(ds);
+    l.add_style("data_style");
+    m.add_layer(std::move(l));
+    
+    m.zoom_all();
+    save_png(m, output_path);
 }
